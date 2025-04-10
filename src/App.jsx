71647efa11c130d3884c5c16c0aa82be72fd2eb7
Varsha Jacob <55629256+VJac15@users.noreply.github.com>
2025-04-10 14:47:42 -0400

    Add initial forecasting data
    We could do something like this for the forecasting data. I think for the other methods, they could be more interactive

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,37 @@ function App() {
 
   return (
     <>
-      <h1>Map</h1>
+      <h1>Analyzing and Forecasting Air Quality in India</h1>
+      <h2>About</h2>
+      <p>Our project approaches this problem through two main areas of focus. We have studied both statistical analysis of air quality data to identify trends across different factors, as well as forecasting future air quality based on current data.</p>
+      <h2>Statistical Analysis</h2>
+      <p>Update the year in the dropdown and interact with the heat map for more information</p>
       <Dropdown />
       <D3Chart />
-      <CorrelationHeatmap />
+      
       <AQILineChart />
+
+      <h2>Forecasting</h2>
+      <p>We used various methods in order to attempt forecasting air quality. The results of each of the methods are shown below.</p>
+      <h3>ARIMA Model</h3>
+
+      <h3>Linear Regression</h3>
+      <img src="/linear_regression.png" alt="Linear Regression" />
+      <p>Data: R-squared: 0.9217139055744
+      Mean Squared Error: 651.3567823009216</p>
+      <h3>Logistic Regression</h3>
+      <img src="/logistic_regression.png" alt="Logistic Regression" />
+
+      <h3>Random Forest</h3>
+      <img src="/random_forest.png" alt="Random Forest" />
+
+      <h3>SVM</h3>
+      <img src="/svm.png" alt="SVM" />
+
+      <h3>Sarima</h3>
+      <p>Still working on this one, could make it interactive possibly</p>
+
+      
       
     </>
   )
